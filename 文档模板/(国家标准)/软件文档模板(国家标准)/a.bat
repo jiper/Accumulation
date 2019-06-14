3baeda00ecf51affdb76aecc9bb4db4530cf30fc
jiper <542062320@qq.com>
2019-06-14 17:52:22 +0800

    6.14

---
+++ 文档模板/(国家标准)/软件文档模板(国家标准)/a.bat
@@ -0,0 +1,18 @@
+Ren 01о棨GB856788.doc	01可行性研究报告.doc
+Ren 02??GB856788.doc	02项目开发计划.doc
+Ren 03?飨GB856788.doc	03软件需求说明书.doc
+Ren 04??飨GB856788.doc	04数据要求说明书.doc
+Ren 05??GB856788.doc	05测试计划.doc
+Ren 06??飨GB856788.doc	06概要设计说明书.doc
+Ren 07??飨GB856788.doc	07详细设计说明书.doc
+Ren 08??飨GB856788.doc	08数据库设计说明书.doc
+Ren 09???GB856788.doc	09用户手册.doc
+Ren 10??GB856788.doc	10操作手册.doc
+Ren 11???GB856788.doc	11模块开发卷宗.doc
+Ren 12?棨GB856788.doc	12测试分析报告.doc
+Ren 13±GB856788.doc	13开发进度月报.doc
+Ren 14???棨GB856788.doc	14项目开发总结报告.doc
+Ren ????GB8567-88.txt	计算机软件产品开发文.doc
+Ren ??.doc	计算机软件需求说明编.doc
+Ren ???涨?GB8567-88.doc	文件给制实施规定的实.doc
+
